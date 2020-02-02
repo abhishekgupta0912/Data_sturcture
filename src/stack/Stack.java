eecad86e368d6e36628b980e6132d0141c69093f
abhishek <abhishek.gupta1_cs18@gla.ac.in>
2020-02-02 17:26:49 +0530

    Stack implementation using linked list

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -16,7 +16,7 @@ class StackException extends Exception {
     }
 }
 
-class Stack {
+public class Stack {
     Node top;
     int len;
 
@@ -58,20 +58,4 @@ class Stack {
 
 }
 
-public class MainStack {
-    public static void main(String[] args) throws StackException {
-        Stack s = new Stack();
-        s.push(56);
-        s.push(23);
-        s.push(564);
-        s.push(213);
-        s.push(5116);
-        s.push(2354);
-        //    System.out.println(s.length());
-        while (s.length() > 0) {
-            System.out.println(s.pop() + " " + s.length());
-        }
 
-
-    }
-}
